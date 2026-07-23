e75f1de7b669cebc9c307658a17dd2c1fbc4b244
Faris Aghna <frsaghna@gmail.com>
2026-07-23 20:45:01 +0700

    fix: Enable edge-to-edge for consistent status bar top padding across all devices

--- app/src/main/java/com/kern/launcher/MainActivity.kt
+++ app/src/main/java/com/kern/launcher/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -30,6 +31,7 @@ class MainActivity : ComponentActivity() {
     private val settingsViewModel: SettingsViewModel by viewModels { factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             val userSettings by homeViewModel.userSettings.collectAsState()
